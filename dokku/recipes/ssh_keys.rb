24be68682c0f00e72a86b6a618fdb3230c8cb8e6
Adrien Brault <adrien.brault@gmail.com>
2014-06-09 11:20:47 -0700

    dokku: Support multiple ssh keys for a user

--- dokku/recipes/ssh_keys.rb
+++ dokku/recipes/ssh_keys.rb
@@ -6,10 +6,12 @@ file "#{node['dokku']['root']}/.ssh/authorized_keys" do
     end
 end
 
-node['dokku']['ssh_keys'].each do |user, key|
-  	# TODO make this into an LWRP
-	execute "sshcommand_acl-add_key" do
-		command "echo '#{key}' | sudo sshcommand acl-add dokku #{user}"
-		cwd node['dokku']['root']
-	end
+node['dokku']['ssh_keys'].each do |user, keys|
+  # TODO make this into an LWRP
+  Array(keys).each do |key|
+    execute "sshcommand_acl-add_key" do
+      command "echo '#{key}' | sudo sshcommand acl-add dokku #{user}"
+      cwd node['dokku']['root']
+    end
+  end
 end
